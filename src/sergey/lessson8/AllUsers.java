301208c27339b3aa946bf9c6b8e48c12bf78532f
Naumenkosergey <43389318+Naumenkosergey@users.noreply.github.com>
2020-01-13 12:36:54 +0300

    registration

---
+++ src/sergey/lessson8/AllUsers.java
@@ -0,0 +1,45 @@
+package sergey.lessson8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AllUsers {
+    private AllUsers() {
+    }
+
+    static ArrayList<String> users = new ArrayList<>(Arrays.asList("admin"));
+    static ArrayList<String> passwords = new ArrayList<>(Arrays.asList("admin"));
+
+
+
+    public static void printUsers() {
+        System.out.println("Все пользователи:");
+        for (int i = 0; i < users.size(); i++) {
+            System.out.println(users.get(i) + " - " + passwords.get(i));
+        }
+    }
+
+    public static void addUser(User user) {
+        users.add(user.name);
+        passwords.add(user.password);
+        System.out.println("Пользователь " + user.name + " успешно добавлен");
+    }
+
+    public static boolean chekUserPassvord(String login, String password) {
+        for (int i = 0; i < users.size(); i++) {
+            if ((users.get(i).equals(login)) && (passwords.get(i).equals(password))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean chekUser(String login) {
+        for (int i = 0; i < users.size(); i++) {
+            if ((users.get(i).equals(login))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/sergey/lessson8/Runner.java
@@ -0,0 +1,31 @@
+package sergey.lessson8;
+
+import java.util.Scanner;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Controller controller = new Controller();
+        while (true) {
+            System.out.println("--------------------------");
+            System.out.println("Выберите действия");
+            System.out.println("1 - войти");
+            System.out.println("2 - зарегистрироваться");
+            System.out.println("3 - вывести всех");
+            System.out.println("0 - завершить");
+            int answer = scanner.nextInt();
+            if (answer == 1)
+                controller.SignIn();
+            else if (answer == 2)
+                controller.signUp();
+            else if (answer == 3)
+                controller.printAllUsers();
+            else if (answer == 0)
+                break;
+            else
+                controller.error();
+        }
+    }
+}
+
